9175d4b5105096c5e5515abf0e797c6f399d3c21
Bayakan <spring.boot@hotmail.com>
2024-03-22 19:47:22 +0300

    Приложение "Список" (Часть 1)

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView plList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "MatLab"};
+        plList.setText("");
+        for (String name : names) {
+            plList.append(name + "\n");
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
     }
 }
